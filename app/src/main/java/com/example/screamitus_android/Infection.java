442b7778f79f1ea6b3d43c1a693b093e2b164545
Himanshu <hmehta095@gmail.com>
2019-09-19 11:08:21 -0400

    TC4 Green

--- app/src/main/java/com/example/screamitus_android/Infection.java
+++ app/src/main/java/com/example/screamitus_android/Infection.java
@@ -12,23 +12,28 @@ public class Infection {
 
             return 0;
         }
-        // TC1 if number of days less than 0
-        if(day <=  0){
-            return -1;
-        }
-
-        // TC2 If instructor rate is 5 per day
-        else if(day >0 && day < 8){
-            return  day*5;
-        }
-
-        //TC3 infection change if day >7
 
         else{
-            int infected = (7*5) + (day-7)*8 ;
-            return  infected;
 
+            // TC1 if number of days less than 0
+            if(day <=  0){
+                return -1;
+            }
+
+            // TC2 If instructor rate is 5 per day
+            else if(day >0 && day < 8){
+                return  day*5;
+            }
+
+            //TC3 infection change if day >7
+
+            else{
+                int infected = (7*5) + (day-7)*8 ;
+                return  infected;
+
+            }
         }
+
 
 
 
